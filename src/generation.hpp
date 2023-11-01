4a9be67e9a719d211c397dc1d405f9b9b9a1df55
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-11-01 22:57:02 +0200

    generator efficancy

--- src/generation.hpp
+++ src/generation.hpp
@@ -51,27 +51,29 @@ public:
     void gen_term(const NodeTerm* term){
         struct TermVisitor {
             Generator& gen;
-            void operator()(const NodeTermIntLit* term_int_lit) const
-            {
-                gen.m_output << "    mov rax, " << term_int_lit->int_lit.value.value() << "\n";
-                gen.push("rax");
+            
+            void operator()(const NodeTermIntLit* term_int_lit) const{
+              gen.m_output << "    mov rax, " << term_int_lit->int_lit.value.value() << "\n";
+              gen.push("rax");
             }
-            void operator()(const NodeTermIdent* term_ident) const
-            {
-                auto it = std::find_if(gen.m_vars.cbegin(), gen.m_vars.cend(), [&](const Var& var) {
-                    return var.name == term_ident->ident.value.value();
-                });
-                if (it == gen.m_vars.cend()) {
-                    std::cerr << "Undeclared identifier: " << term_ident->ident.value.value() << std::endl;
-                    exit(EXIT_FAILURE);
-                }
-                std::stringstream offset;
-                offset << "QWORD [rsp + " << (gen.m_stack_size - (*it).stack_loc - 1) * 8 << "]";
-                gen.push(offset.str());
+
+            // If we need to use a variable, extract the value of the variable and put it at the top of the stack
+            void operator()(const NodeTermIdent* term_ident) const{
+              auto it = std::find_if(gen.m_vars.cbegin(), gen.m_vars.cend(), [&](const Var& var) {
+                return var.name == term_ident->ident.value.value(); 
+              });
+              if (it == gen.m_vars.cend()) {
+                std::cerr << "Undeclared identifier: " << term_ident->ident.value.value() << std::endl;
+                exit(EXIT_FAILURE);
+              }
+              std::stringstream offset;
+              // push the offset of the variable on the stack
+              offset << "QWORD [rsp + " << (gen.m_stack_size - (*it).stack_loc - 1) * 8 << "]";
+              gen.push(offset.str());
             }
             void operator()(const NodeTermParen* term_paren) const
             {
-                gen.gen_expr(term_paren->expr);
+              gen.gen_expr(term_paren->expr);
             }
         };
         TermVisitor visitor({ .gen = *this });
@@ -122,8 +124,9 @@ public:
         BinExprVisitor visitor { .gen = *this };
         std::visit(visitor, bin_expr->var);
     }
-
+ 
     void gen_expr(const NodeExpr* expr){
+
         struct ExprVisitor {
             Generator& gen;
             void operator()(const NodeTerm* term) const
@@ -150,99 +153,97 @@ public:
     }
 
     void gen_stmt(const NodeStmt* stmt){
-        struct StmtVisitor {
-            Generator& gen;
-            void operator()(const NodeStmtExit* stmt_exit) const
-            {
-                gen.gen_expr(stmt_exit->expr);
-                gen.m_output << "    mov rax, 60\n";
-                gen.pop("rdi");
-                gen.m_output << "    syscall\n";
-            }
-            void operator()(const NodeStmtLet* stmt_let) const
-            {
-                auto it = std::find_if(gen.m_vars.cbegin(), gen.m_vars.cend(), [&](const Var& var) {
-                    return var.name == stmt_let->ident.value.value();
-                });
-                if (it != gen.m_vars.cend()) {
-                    std::cerr << "Identifier already used: " << stmt_let->ident.value.value() << std::endl;
-                    exit(EXIT_FAILURE);
-                }
-                gen.m_vars.push_back({ .name = stmt_let->ident.value.value(), .stack_loc = gen.m_stack_size });
-                gen.gen_expr(stmt_let->expr);
-            }
-            void operator()(const NodeScope* scope) const
-            {
-                gen.gen_scope(scope);
-            }
-            void operator()(const NodeStmtIf* stmt_if) const
-            {
-                gen.gen_expr(stmt_if->expr);
-                gen.pop("rax");
-                std::string label = gen.create_label();
-                gen.m_output << "    test rax, rax\n";
-                gen.m_output << "    jz " << label << "\n";
-                gen.gen_scope(stmt_if->scope);
-                gen.m_output << label << ":\n";
-            }
-        };
 
-        StmtVisitor visitor { .gen = *this };
-        std::visit(visitor, stmt->var);
+      struct StmtVisitor {
+          Generator& gen;
+          void operator()(const NodeStmtExit* stmt_exit) const
+          {
+            gen.gen_expr(stmt_exit->expr);
+            gen.m_output << "    mov rax, 60\n";
+            gen.pop("rdi"); // pop from the stack and put it in rdi
+            gen.m_output << "    syscall\n";
+          }
+
+          void operator()(const NodeStmtLet* stmt_let) const{
+            auto it = std::find_if(gen.m_vars.cbegin(), gen.m_vars.cend(), [&](const Var& var) {
+              return var.name == stmt_let->ident.value.value();
+            });
+
+            if (it != gen.m_vars.cend()) {
+              std::cerr << "Identifier already used: " << stmt_let->ident.value.value() << std::endl;
+              exit(EXIT_FAILURE);
+            }
+            // the stack location
+            gen.m_vars.push_back({ .name = stmt_let->ident.value.value(), .stack_loc = gen.m_stack_size });
+            gen.gen_expr(stmt_let->expr);
+          }
+          void operator()(const NodeScope* scope) const
+          {
+            gen.gen_scope(scope);
+          }
+          void operator()(const NodeStmtIf* stmt_if) const
+          {
+            gen.gen_expr(stmt_if->expr);
+            gen.pop("rax");
+            std::string label = gen.create_label();
+            gen.m_output << "    test rax, rax\n";
+            gen.m_output << "    jz " << label << "\n";
+            gen.gen_scope(stmt_if->scope);
+            gen.m_output << label << ":\n";
+          }
+      };
+
+      StmtVisitor visitor { .gen = *this };
+      std::visit(visitor, stmt->var);
     }
 
     [[nodiscard]] std::string gen_prog(){
-        m_output << "global _start\n_start:\n";
+      m_output << "global _start\n_start:\n";
 
-        for (const NodeStmt* stmt : m_prog.stmts) {
-            gen_stmt(stmt);
-        }
+      for (const NodeStmt* stmt : m_prog.stmts) {
+        gen_stmt(stmt);
+      }
 
-        m_output << "    mov rax, 60\n";
-        m_output << "    mov rdi, 0\n";
-        m_output << "    syscall\n";
-        return m_output.str();
+      m_output << "    mov rax, 60\n";
+      m_output << "    mov rdi, 0\n";
+      m_output << "    syscall\n";
+      return m_output.str();
     }
 
 private:
-    void push(const std::string& reg)
-    {
-        m_output << "    push " << reg << "\n";
-        m_stack_size++;
+    void push(const std::string& reg){
+      m_output << "    push " << reg << "\n";
+      m_stack_size++;
     }
 
-    void pop(const std::string& reg)
-    {
-        m_output << "    pop " << reg << "\n";
-        m_stack_size--;
+    void pop(const std::string& reg){
+      m_output << "    pop " << reg << "\n";
+      m_stack_size--;
     }
 
-    void begin_scope()
-    {
-        m_scopes.push_back(m_vars.size());
+    void begin_scope(){
+      m_scopes.push_back(m_vars.size());
     }
 
-    void end_scope()
-    {
-        size_t pop_count = m_vars.size() - m_scopes.back();
-        m_output << "    add rsp, " << pop_count * 8 << "\n";
-        m_stack_size -= pop_count;
-        for (int i = 0; i < pop_count; i++) {
-            m_vars.pop_back();
-        }
-        m_scopes.pop_back();
+    void end_scope(){
+      size_t pop_count = m_vars.size() - m_scopes.back();
+      m_output << "    add rsp, " << pop_count * 8 << "\n";
+      m_stack_size -= pop_count;
+      for (int i = 0; i < pop_count; i++) {
+        m_vars.pop_back();
+      }
+      m_scopes.pop_back();
     }
 
-    std::string create_label()
-    {
-        std::stringstream ss;
-        ss << "label" << m_label_count++;
-        return ss.str();
+    std::string create_label(){
+      std::stringstream ss;
+      ss << "label" << m_label_count++;
+      return ss.str();
     }
 
     struct Var {
-        std::string name;
-        size_t stack_loc;
+      std::string name;
+      size_t stack_loc;
     };
 
     const NodeProg m_prog;
